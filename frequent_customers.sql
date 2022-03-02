29b0d78c7cd492893d2ab421e331675a57639ebc
AditiNanoti <100706881+AditiNanoti@users.noreply.github.com>
2022-03-02 13:59:52 +0530

    Update frequent_customers.sql

--- frequent_customers.sql
+++ frequent_customers.sql
@@ -4,3 +4,4 @@ FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801` gs17
 INNER JOIN   `bigquery-public-data.google_analytics_sample.ga_sessions_20160801` gs16 ON
 gs17.geoNetwork.region=gs16.geoNetwork.region
 GROUP BY gs17.geoNetwork.region
+ORDER BY VISITORS asc
