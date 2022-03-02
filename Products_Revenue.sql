f6233bda246141c4ec17732ad6d80c650b3c317a
AditiNanoti <100706881+AditiNanoti@users.noreply.github.com>
2022-03-02 13:46:56 +0530

    Update Products_Revenue.sql

--- Products_Revenue.sql
+++ Products_Revenue.sql
@@ -6,3 +6,4 @@ FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
   _TABLE_SUFFIX BETWEEN '20160701'
   AND '20170701'AND (p.productRevenue IS NOT NULL )
   GROUP BY v2ProductName, productPrice, productQuantity
+  ORDER BY Revenue DESC
